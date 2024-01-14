10434c65e747d409f3dad328508ac86ce1e455e9
Shin'ya Ueoka <ueokande@i-beam.org>
2024-01-14 06:12:22 +0000

    feat: support loading content script of manifest v3 partially

--- src/firefox_addon_installer.ts
+++ src/firefox_addon_installer.ts
@@ -1,10 +1,16 @@
 import * as remote from "./firefox_remote";
 
+type InstallTemporaryAddonResponse = {
+  addon: {
+    id: string;
+  };
+};
+
 export class FirefoxAddonInstaller {
   constructor(private readonly debuggingServerPort: number) {}
 
-  async install(path: string): Promise<void> {
+  async install(path: string): Promise<InstallTemporaryAddonResponse> {
     const client = await remote.connect(this.debuggingServerPort);
-    await client.installTemporaryAddon(path);
+    return client.installTemporaryAddon(path);
   }
 }
